45396991df26b4f79fa5489f57e752cc248861a0
krowbee <skoresplay@gmail.com>
2026-03-15 21:19:26 +0200

    fix: fixed user domain names

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './domains/users/users.controller';
-import { UsersService } from './domains/users/users.service';
-import { UsersModule } from './domains/users/users.module';
+import { UsersController } from './domains/user/user.controller';
+import { UsersService } from './domains/user/user.service';
+import { UsersModule } from './domains/user/user.module';
 import { AuthController } from './domains/auth/auth.controller';
 import { AuthService } from './domains/auth/auth.service';
 import { AuthModule } from './domains/auth/auth.module';
