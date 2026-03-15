27f35e77b66e080ccb1e723ba17b07eaa2f90a04
krowbee <skoresplay@gmail.com>
2026-03-16 00:32:14 +0200

    feat: add prisma extension for password hashing, changed dependency in prisma module

--- src/lib/prisma/prisma.module.ts
+++ src/lib/prisma/prisma.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { CryptoModule } from '../crypto/crypto.module';
 
+@Global()
 @Module({
+  imports: [CryptoModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
--- src/lib/prisma/prisma.service.ts
+++ src/lib/prisma/prisma.service.ts
@@ -1,17 +1,39 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
+import { CryptoService } from '../crypto/crypto.service';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private cryptoService: CryptoService) {
     const adapter: PrismaPg = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
 
     super({ adapter });
+    this.$extends({
+      query: {
+        user: {
+          async create({ args, query }) {
+            if (typeof args.data.password === 'string') {
+              args.data.password = await cryptoService.hashValue(
+                args.data.password,
+              );
+            }
+            return query(args);
+          },
+        },
+      },
+    });
   }
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
