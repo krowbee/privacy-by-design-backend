405974881f5c63de10b65786e25f678ae9fdec1d
krowbee <skoresplay@gmail.com>
2026-03-15 21:56:24 +0200

    feat: add crypto service, add models to prisma schema

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { SignInDto, SignUpDto } from './auth.dto';
+import { PrismaService } from 'src/lib/prisma/prisma.service';
 @Injectable()
 export class AuthService {
+  constructor(private prismaService: PrismaService) {}
   async loginUser(data: SignInDto) {}
 
   async registerUser(data: SignUpDto) {}
