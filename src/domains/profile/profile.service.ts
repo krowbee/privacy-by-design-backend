98324c7c6d963032c385fc23191d33cb2f340830
krowbee <skoresplay@gmail.com>
2026-03-20 17:47:21 +0200

    feat: add create and update profile methods

--- src/domains/profile/profile.service.ts
+++ src/domains/profile/profile.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { CreateProfileDTO, UpdateProfileDTO } from './types/profile.dto';
+import { PrismaService } from 'src/lib/prisma/prisma.service';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(private prisma: PrismaService) {}
+  async createProfile(data: CreateProfileDTO, userId: string) {
+    const isExists = await this.prisma.client.profile.findUnique({
+      where: { userId },
+    });
+    if (isExists) throw new ForbiddenException('You already have profile');
+    const profile = this.prisma.client.profile.create({
+      data: { ...data, userId },
+    });
+    return profile;
+  }
+
+  async updateProfile(data: UpdateProfileDTO, userId: string) {
+    const isExists = await this.prisma.client.profile.findUnique({
+      where: { userId },
+    });
+    if (!isExists) throw new ForbiddenException("You don't have profile");
+    const profile = await this.prisma.client.profile.update({
+      where: { userId },
+      data,
+    });
+    return profile;
+  }
+}
