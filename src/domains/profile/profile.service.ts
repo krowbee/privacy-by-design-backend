f62c1aa559418e09823724fbd29c96ddf35a2679
krowbee <skoresplay@gmail.com>
2026-03-22 18:47:24 +0200

    feat: add getProfile function

--- src/domains/profile/profile.service.ts
+++ src/domains/profile/profile.service.ts
@@ -1,6 +1,11 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProfileDTO, UpdateProfileDTO } from './types/profile.dto';
 import { PrismaService } from 'src/lib/prisma/prisma.service';
+import { maskNumbers } from './utills/passportNumberMask';
 
 @Injectable()
 export class ProfileService {
@@ -14,18 +19,40 @@ export class ProfileService {
     const profile = await this.prisma.client.profile.create({
       data: { ...data, userId },
     });
-    return profile;
+    const passportNumber = maskNumbers(profile.passportNumber);
+    return { ...profile, passportNumber };
   }
 
   async updateProfile(data: UpdateProfileDTO, userId: string) {
     const isExists = await this.prisma.client.profile.findUnique({
       where: { userId },
     });
-    if (!isExists) throw new ForbiddenException("You don't have profile");
+    if (!isExists) throw new NotFoundException("You don't have profile");
     const profile = await this.prisma.client.profile.update({
       where: { userId },
       data,
     });
+    const passportNumber = maskNumbers(profile.passportNumber);
+    return { ...profile, passportNumber };
+  }
+
+  async getProfile(userId: string) {
+    const profile = await this.prisma.client.profile.findUnique({
+      where: { userId },
+    });
+    if (!profile) throw new NotFoundException("You don't have profile");
     return profile;
+  }
+
+  async deleteProfile(userId: string) {
+    const isExists = await this.prisma.client.profile.findUnique({
+      where: { userId },
+    });
+    if (!isExists) throw new NotFoundException("You don't have profile");
+    const profile = await this.prisma.client.profile.delete({
+      where: { userId },
+    });
+    const passportNumber = maskNumbers(profile.passportNumber);
+    return { ...profile, passportNumber };
   }
 }
