eb693b46fb1ef99921af027cbf8c9b9aab0a44b4
krowbee <skoresplay@gmail.com>
2026-03-22 19:01:02 +0200

    feat: add get, patch, delete functions to profile controller

--- src/domains/profile/profile.controller.ts
+++ src/domains/profile/profile.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { CreateProfileDTO } from './types/profile.dto';
+import { CreateProfileDTO, UpdateProfileDTO } from './types/profile.dto';
 import { AuthOnlyGuard } from '../auth/guards/auth-only.guard';
 import { User } from '../auth/decorators/user.decorator';
 import type { TokenPayload } from '../auth/types/payload';
@@ -10,6 +18,14 @@ import { ProfileService } from './profile.service';
 @Controller('/profile')
 export class ProfileController {
   constructor(private profileService: ProfileService) {}
+
+  @Get('')
+  @UseGuards(AuthOnlyGuard)
+  async getProfile(@User() user: TokenPayload) {
+    const profile = await this.profileService.getProfile(user.id);
+    return profile;
+  }
+
   @Post('')
   @UseGuards(AuthOnlyGuard)
   async createProfile(
@@ -17,6 +33,23 @@ export class ProfileController {
     @User() user: TokenPayload,
   ) {
     const profile = await this.profileService.createProfile(data, user.id);
-    return profile;
+    return { id: profile.id };
+  }
+
+  @Patch('')
+  @UseGuards(AuthOnlyGuard)
+  async updateProfile(
+    @Body() data: UpdateProfileDTO,
+    @User() user: TokenPayload,
+  ) {
+    await this.profileService.updateProfile(data, user.id);
+    return { message: 'Profile updated successfully' };
+  }
+
+  @Delete('')
+  @UseGuards(AuthOnlyGuard)
+  async deleteProfile(@User() user: TokenPayload) {
+    await this.profileService.deleteProfile(user.id);
+    return { message: 'Profile deleted successfully' };
   }
 }
