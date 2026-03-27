1a496e6934c3f3b79207010c06ec2f1b3fccd1c4
krowbee <skoresplay@gmail.com>
2026-03-27 22:25:17 +0200

    feat: add nestjs-cls. setup global nstcjs middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,31 @@ import { AppService } from './app.service';
 import { AuthModule } from './domains/auth/auth.module';
 import { PrismaModule } from './lib/prisma/prisma.module';
 import { ProfileModule } from './domains/profile/profile.module';
+import { ClsModule } from 'nestjs-cls';
+import { randomUUID } from 'crypto';
+import { Request } from 'express';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ProfileModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    ProfileModule,
+    ClsModule.forRoot({
+      global: true,
+      middleware: {
+        mount: true,
+        generateId: true,
+        idGenerator: (req: Request) =>
+          (req.headers['x-request-id'] as string) || randomUUID(),
+        setup: (cls, req: Request) => {
+          cls.set('ip', req.ip);
+          cls.set('method', req.method);
+          cls.set('path', req.originalUrl);
+          cls.set('userAgent', req.headers['user-agent']);
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
